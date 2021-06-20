4796a993ec89c26323ddfc4654984e4fa51dd0d5
thomasdw33 <56744083+thomasdw33@users.noreply.github.com>
2021-06-20 11:45:34 -0400

    Updated events/leadership page
    - Updated how featured events are added, should now be done automatically (featured key added to events.json)
    - Added skeleton for doing the same with the home page events (not implemented)
    - Fixed bug with events page sites going to show
    - Updated leadership page

--- assets/js/orderEvents.js
+++ assets/js/orderEvents.js
@@ -9,12 +9,15 @@ JSON structure:
   startDate: **required**
   endDate: not required
   description: not required
+  featured: **required**
 */
 
 const url = 'assets/events.json';
 const norelblank = "target=\"_blank\" rel=\"noopener noreferrer\"";
 let pastEvents = [];
 let upcomingEvents = [];
+let featuredEvents = [];
+let homeEvents = [];
 
 $.getJSON(url)
   .done(data => {
@@ -31,6 +34,13 @@ function processEvents(data) {
   insertEventsIntoPage(); //ensure this works for featured events
 }
 
+/**
+ Splits events coming from data stream into two stacks, upcomingEvents and pastEvents, and
+ adds featured events onto the featuredEvents stack
+ @param data
+    json file with events
+ @requires [required fields are filled in on json file]
+*/
 function splitEventsIntoPastAndUpcoming(data) {
   // Sort the events into past and upcoming.
   const currentDate = new Date();
@@ -40,6 +50,11 @@ function splitEventsIntoPastAndUpcoming(data) {
       pastEvents.push(event);
     } else {
       upcomingEvents.push(event);
+    }
+    //add featured events
+    const featured = event.featured;
+    if(featured){
+        featuredEvents.push(event);
     }
   }
 }
@@ -62,6 +77,9 @@ function orderPastAndUpcomingEventsChronologically() {
   });
 }
 
+/**
+ Inserts upcoming, past, and featured events into the homepage
+*/
 function insertEventsIntoPage() {
   for (const event of upcomingEvents) {
     $("#events-upcoming").append(createEventBlock(event));
@@ -69,10 +87,24 @@ function insertEventsIntoPage() {
   for (const event of pastEvents) {
     $("#events-past").append(createEventBlock(event));
   }
-  //Add for featured event
+  for (const event of featuredEvents){
+    $("#featured-event").append(createFeaturedEventBlock(event));
+  }
+ // for (const event of homeEvents){
+ //   #("#event-box-text").append(createHomeEventBlock(event));
+ // }
 }
 
-
+/*
+    event.
+        eventName: string of the event name
+        site: string to the link of event page
+        startDate: string of the start date (ex. "March 1, 2021")
+        endDate: string of the end date (ex. "March 5, 2021")
+        location: string of the location
+        description: string of an event description
+        featured: boolean if event is featured (one at a time)
+*/
 function createEventBlock(event) {
   let elem = "";
   if (event.site) {
@@ -109,32 +141,78 @@ function createEventBlock(event) {
 
 
 
-//Creates a featured event block
+/**
+ Creates a featured event block
+*/
 function createFeaturedEventBlock(event){
   let elem = "";
-  if (event.site) {
-      elem += "<a href=\"" + event.site + "\" " + norelblank + "><h3>FEATURED EVENT></h3>\n" + "<h1>" + event.eventName + "</h1>\n";
-    } else {
-      elem += "<h3>FEATURED EVENT></h3>\n" + "<h1>" + event.eventName + "</h1>\n";
-    }
-
-
-    if (event.endDate) {
-        const start = event.startDate.split(" ");
-        const end = event.endDate.split(" ");
-        if (start[0] == end[0]){
-          elem += "<h4>" + start[0] + " " + start[1].substring(0, start[1].length - 1) + " - " + end[1].substring(0, end[1].length - 1) + ", " + start[2] + "</h4>\n";
+  if(event.featured){
+      if (event.site) {
+          elem += "<a href=\"" + event.site + "\" " + norelblank + "><h3>FEATURED EVENT</h3>\n" + "<h1>" + event.eventName + "</h1>\n";
         } else {
-          elem += "<h4>" + start[0] + " " + start[1].substring(0, start[1].length - 1) + " - " + end[0] + " " + end[1].substring(0, end[1].length - 1) + ", " + start[2] + "</h4>\n";
+          elem += "<h3>FEATURED EVENT</h3>\n" + "<h1>" + event.eventName + "</h1>\n";
         }
-      } else {
-        elem += "<h4>" + event.startDate + "</h4>\n";
-      }
 
-      if(event.site){
-        elem += "</a>";
-      }
 
+        if (event.endDate) {
+            const start = event.startDate.split(" ");
+            const end = event.endDate.split(" ");
+            if (start[0] == end[0]){
+              elem += "<h4>" + start[0] + " " + start[1].substring(0, start[1].length - 1) + " - " + end[1].substring(0, end[1].length - 1) + ", " + start[2] + "</h4>\n";
+            } else {
+              elem += "<h4>" + start[0] + " " + start[1].substring(0, start[1].length - 1) + " - " + end[0] + " " + end[1].substring(0, end[1].length - 1) + ", " + start[2] + "</h4>\n";
+            }
+          } else {
+            elem += "<h4>" + event.startDate + "</h4>\n";
+          }
+
+          if(event.site){
+            elem += "</a>";
+          }
+
+    }
 
   return elem;
 }
+
+/*
+//Creates a home event block
+function createHomeEventBlock(event){
+  let elem = "";
+  var homeDate = new Date("February 9, 2012, 12:15");
+  var currentdate = new Date();
+
+  let name = event.eventName;
+      switch(name.substring(0,name.indexOf("I/O")){
+        case "HackOHI/O":
+        //code
+        break;
+        case "MakeOHI/O":
+        //code
+        break;
+        case "ShowOHI/O"
+
+        break;
+        case "Code I/O"
+
+        break;
+        case "High School I/O"
+        break;
+
+        case "Data I/O"
+        break;
+
+        case "Hack A/I"
+        break;
+
+        case "Logi I/O"
+        break;
+
+        case "Bio-Hack"
+        break;
+
+      }
+
+  return elem;
+}*/
+
